8152bd4014d893e3b348740f50dd105f47d09d47
Justeen Lee <justeenlee@gatech.edu>
2016-11-02 14:44:59 -0400

    add mapAR unity

---
+++ map unity/map vuforia/Assets/buttonAScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class buttonAScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject buttonA;
+	private GameObject buttonARep;
+	// Use this for initialization
+	void Start () {
+		buttonA = GameObject.Find ("buttonA");
+		buttonARep = GameObject.Find ("buttonARep");
+		buttonA.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+
+		Debug.Log ("============================= everything set up!!!");
+		Debug.Log ("================= color ="+buttonARep.GetComponent<Renderer>().material.color);
+
+	}
+
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour btn) {
+		Debug.Log ("============================= pressed");
+		Debug.Log (btn.VirtualButtonName);
+
+		Color c1 = new Color (0F, 0F, 0F);
+		buttonARep.GetComponent<Renderer> ().material.color = c1;
+	}
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour btn) {
+		Debug.Log ("============================= released");
+
+		buttonARep.GetComponent<Renderer>().material.color = new Color (255F, 255F, 255F);
+	}
+
+}
---
+++ map unity/map vuforia/Assets/navigationScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class navigationScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+
+	private GameObject location1btn;
+	private GameObject location1rep;
+
+	// Use this for initialization
+	void Start () {
+		location1btn = GameObject.Find ("location1btn");
+		location1rep = GameObject.Find ("location1rep");
+		location1btn.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+
+	}
+	
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour btn) {
+		Debug.Log ("============================= navigation pressed");
+		Debug.Log (btn.VirtualButtonName);
+
+		Color c1 = new Color (0F, 0F, 0F);
+		location1rep.GetComponent<Renderer> ().material.color = c1;
+	}
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour btn) {
+		Debug.Log ("============================= navigation released");
+
+		location1rep.GetComponent<Renderer>().material.color = new Color (255F, 255F, 255F);
+	}
+
+}
